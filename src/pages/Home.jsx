015f06944b89903ad1865e12023a7e4f04cc9c9c
advait-soni <advaitsoni2020@gmail.com>
2025-05-04 17:28:30 +0530

    commit message

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,10 @@
+import { useState } from "react"
 
-export default function Home(){
-    return (
-        <div>
-            <p>Home Page</p>
-        </div>
-    )
+export default function Home() {
+
+  return (
+    <div>
+
+    </div>
+  )
 }
